d3304e08fd67f9f3b40333c1ab8218782433e767
jakaria242 <jakaria.dev242@gmail.com>
2024-11-10 23:51:43 +0600

    jwt verification problem solve & verify

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,41 +2,50 @@ import { User } from '../models/userSchema.model.js'
 import { mail } from '../utils/sendMail.js'
 import verifyEmailTemplate from '../mailTemplate/verifyEmailTemplate.js'
 
+// @desc create a user
+// route POST /api/v1/user
 const createUser = async (req, res) => {
-  const { fullName, phoneNumber, email, password } = req.body
+  try {
+    const { fullName, phoneNumber, email, password } = req.body
 
-  const isFound = await User.findOne({ email })
-  if (isFound) {
-    return res.json('email already exists')
+    // check user already registered or not
+    const isFound = await User.findOne({ email })
+    if (isFound) {
+      return res.json('email already exists')
+    }
+
+    const user = await User.create({ fullName, phoneNumber, email, password })
+    const link = await user.generateAccesstoken()
+
+    await mail(
+      user.email,
+      'verifycation',
+      'Verify your mail',
+      verifyEmailTemplate(link)
+    )
+
+    return res.json('ok')
+  } catch (error) {
+    console.log('create user error', error)
   }
-
-  const user = await User.create({ fullName, phoneNumber, email, password })
-  const link = await user.generateAccesstoken()
-
-  await mail(
-    user.email,
-    'verifycation',
-    'Verify your mail',
-    verifyEmailTemplate(link)
-  )
-
-  return res.json('ok')
 }
 
 const emailVerify = async (req, res) => {
   try {
     const { link } = req.params
     const user = new User()
-    const { email } = await user.verifyAccessToken(link)
-    if (email) {
-      const userFound = await User.findOne({ email })
+    const token = await user.verifyAccessToken(link)
+    if (token) {
+      const userFound = await User.findOne({ email: token.email })
       if (userFound) {
-       return res.send('verified')
+        userFound.emailVerified = new Date().toDateString()
+        await userFound.save()
+        return res.send('Your email has been verified!')
       } else {
-       return res.send('invalid')
+        return res.send('User verification failed!')
       }
     } else {
-     return res.send('invalid url')
+      return res.send('Invalid verification url')
     }
   } catch (error) {
     console.log('emailVerify error', error)
